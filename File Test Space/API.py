9736b8195fc7e6e4eb237e025e4828a6418fcf77
Akeoott <amejanin00@gmail.com>
2025-04-23 04:02:43 +0200

    Update API.py to replace hardcoded URL and API key with placeholders for better usability

--- File Test Space/API.py
+++ File Test Space/API.py
@@ -1,10 +1,13 @@
 import requests
 
-url = "https://www.virustotal.com/api/v3/files/60c7225ef52d2a5a77463c3450a56c67a0af99dc9a4c337f06a18f11b702d2ff"
+# Replace <Replace with MD5, SHA-1 or SHA-256 Hash> with the actual hash of the file you want to pull information from.
+url = "https://www.virustotal.com/api/v3/files/<Replace with MD5, SHA-1 or SHA-256 Hash>"
 
 headers = {
     "accept": "registrydictionary/json",
-    "x-apikey": "0ebd6106b59598f1b56ba336c1b2bf36fecfac7c37d310d0218095283d316000"
+
+    # Replace <Your-API-Key> with your actual VirusTotal API key.
+    "x-apikey": "<Your API Key>"
 }
 
 response = requests.get(url, headers=headers)
