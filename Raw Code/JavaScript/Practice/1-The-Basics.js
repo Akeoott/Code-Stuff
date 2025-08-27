921efc11f159d2c0de18b28dd430154c35a5956d
Akeoott <amejanin00@gmail.com>
2025-08-27 08:50:39 +0200

    Added initial implementation of the JS tab. Html and css is ready to use JS now.

---
+++ Raw Code/JavaScript/Practice/1-The-Basics.js
@@ -0,0 +1,17 @@
+/*
+    This
+    Is
+    A
+    Comment
+*/
+
+// Logs stuff to the console in the browser! (i love logging)
+console.log("BANANA! Im a log.");
+console.error("FUK UUUUU!");
+
+/* Gives window alert:
+
+window.alert("BENBJFAsbjk")
+window.alert("NIGGGERRRRRR")
+
+*/
