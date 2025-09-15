8469dbc13a9209e97b905860e0c3d9f52ca81204
Akeoott <amejanin00@gmail.com>
2025-09-15 09:49:17 +0200

    Changed C# Project aka learning settings

--- Raw Code/C#/ConsoleApp1/obj/Debug/net9.0/ConsoleApp1.AssemblyInfo.cs
+++ Raw Code/C#/ConsoleApp1/obj/Debug/net9.0/ConsoleApp1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1c31f8a81b576f53ecb1bdd1469b50a8312da000")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cf918eef3e55dccbbfd13f4a34070127b9dec402")]
 [assembly: System.Reflection.AssemblyProductAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
