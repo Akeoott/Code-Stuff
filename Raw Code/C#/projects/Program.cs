928d50c041c2adfcdce9664081d97bfbeffca5a7
Akeoott <amejanin00@gmail.com>
2025-10-27 02:20:18 +0100

    feat: For C#, added initial structure for projects to practice for me and updated .editorconfig.

---
+++ Raw Code/C#/projects/Program.cs
@@ -0,0 +1,2 @@
+﻿// Copyright (c) Akeoott <amejanin00@gmail.com>. Licensed under the MIT License.
+// See the LICENSE file in the repository root for full license text.
---
+++ Raw Code/C#/projects/helper/FormatStr.cs
@@ -0,0 +1,2 @@
+// Copyright (c) Akeoott <amejanin00@gmail.com>. Licensed under the MIT License.
+// See the LICENSE file in the repository root for full license text.
