f0bcab6cf2bca7bbbe91fe71b20b309cb482c64e
Akeoott <amejanin00@gmail.com>
2025-11-02 17:53:43 +0100

    feat(projects): form initial layout for password generator

--- Raw Code/C#/projects/_PasswordGen/PasswordGen.cs
+++ Raw Code/C#/projects/_PasswordGen/PasswordGen.cs
@@ -1,13 +1,35 @@
 // Copyright (c) Akeoott <amejanin00@gmail.com>. Licensed under the LGPL-3.0 License.
 // See the LICENSE file in the repository root for full license text.
 
+using System.Globalization;
+
 namespace Program;
 
-internal static class PasswordGen
+abstract internal class PasswordGen
 {
+    const string Header =
+        """
+        ====================================
+        | Projects by Ame (Akeoott/Akeoot) |
+        ====================================
+        =======| Password Generator |=======
+        ====================================
+
+        """;
+
+    const string Hello =
+        """
+        Hello, World!
+        This is a password generator made by Akeoott!
+
+        """;
+
     internal static void RunPasswordGen()
     {
-        Console.WriteLine("=== Password Generator ===");
-        Console.WriteLine("This project is not implemented yet!");
+        Console.WriteLine(Header);
+        Console.WriteLine(Hello);
+
+        // TODO: Add password generator
+
     }
 }
