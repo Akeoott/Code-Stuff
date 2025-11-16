54e6839865399348153f2741d2701731c9d6466e
Akeoott <akeoot@pm.me>
2025-11-16 17:31:56 +0100

    Reapply "feat(Avalonia): Initialize Avalonia application with MVVM structure from template"
    This reverts commit 98d9f6784e0e525ebd7167047271e719219f22c6.

---
+++ Raw Code/C#/AvaloniaApplication/ViewModels/ViewModelBase.cs
@@ -0,0 +1,10 @@
+﻿// Copyright (c) Ame aka Akeoott <ame@akeoot.org>. Licensed under the LGPL-3.0 License.
+// See the LICENSE file in the repository root for full license text.
+
+using CommunityToolkit.Mvvm.ComponentModel;
+
+namespace AvaloniaApplication.ViewModels;
+
+public abstract class ViewModelBase : ObservableObject
+{
+}
